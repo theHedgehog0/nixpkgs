26dc594bbe38376bbcbe1abc6500dfd9708fc166
Pyrox <pyrox@pyrox.dev>
2024-11-02 19:59:13 -0400

    darker: init at 2.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2866,6 +2866,8 @@ self: super: with self; {
 
   darkdetect = callPackage ../development/python-modules/darkdetect { };
 
+  darker = callPackage ../development/python-modules/darker { };
+
   darkgraylib = callPackage ../development/python-modules/darkgraylib { };
 
   dasbus = callPackage ../development/python-modules/dasbus { };
