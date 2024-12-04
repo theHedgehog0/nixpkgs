c0fd2ff7086bba3e336ece97161b661bdc188f3e
dish <pyrox@pyrox.dev>
2024-12-04 15:12:28 -0500

    nodePackages.@tailwindcss/aspect-ratio: drop

--- pkgs/development/node-packages/aliases.nix
+++ pkgs/development/node-packages/aliases.nix
@@ -48,6 +48,7 @@ mapAliases {
   "@mermaid-js/mermaid-cli" = pkgs.mermaid-cli; # added 2023-10-01
   "@nerdwallet/shepherd" = pkgs.shepherd; # added 2023-09-30
   "@nestjs/cli" = pkgs.nest-cli; # Added 2023-05-06
+  "@tailwindcss/aspect-ratio" = throw "@tailwindcss/aspect-ratio has been removed. It should be installed with your preferred JS package manager instead";
   "@tailwindcss/language-server" = pkgs.tailwindcss-language-server; # added 2024-01-22
   "@volar/vue-language-server" = pkgs.vue-language-server; # added 2024-06-15
   "@vue/language-server" = pkgs.vue-language-server; # added 2024-06-15
